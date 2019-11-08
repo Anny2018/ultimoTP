a935c5c455995344f15d02765cb86188b02d6038
ana <vane2013sanchez@gmail.com>
2019-11-08 09:00:13 -0300

    nuevosCambios

---
+++ components/citiesRouter/citiesRouter.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const citiesModel = require('../citiesModel/citiesModel');
+
+router.get('/city',(req, res) => {
+    citiesModel.find((err, cities) => {
+        if (err) {
+            console.log('Error find users: ', error);
+            return res.status(500).json({ err: 'Error al consultar usuarios.'})
+        }
+        res.status(200).json({ cities: cities });
+    });
+});
+
+router.post('/city', (req, res) => {
+    const newCity = new citiesModel(req.body);
+    newCity.save((err, citySaved) => {
+        if (err) {
+            console.log('Error save user: ', error);
+            return res.status(500).json({ err: 'Error al guardar usuario.'})
+        }
+        res.status(200).json({ newCity: citySaved});
+    })
+});
+
+router.put('/city/:id',(req,res) =>{
+    citiesModel.findByIdAndUpdate(req.params.id, req.body, (err, updated) => {
+        if (err) {
+            console.log('Error update cities: ', error);
+            return res.status(500).json({ err: 'Error al actualizar usuario.'})
+        }
+        res.status(200).json({ cityUpdated: updated });
+    });
+})
+
+router.delete('/city/:id',(req,res) => {
+    citiesModel.findByIdAndRemove(req.params.id, (err, deleted) => {
+        if (err) {
+            console.log('Error delete user: ', error);
+            return res.status(500).json({ err: 'Error al borrar usuario.'})
+        }
+        res.status(200).json({ cityDeleted: deleted });
+    });
+});
+
+module.exports = router;
---
+++ mern-master/src/components/cities.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+
+export default class Cities extends React.Component {
+    state ={
+        listaPaises: []
+    }
+  
+    async componentDidMount(){
+      const res =  await axios.get('http://localhost:5000/api/city');
+      this.setState({listaPaises:res.data.cities});
+      console.log(this.state.listaPaises.cities);
+    }
+
+
+    render() {
+        return(
+     /*  <div>
+        <h1>Lista de paises</h1>
+        <ul>
+          {this.state.listaPaises.map((elem,i)=>{return <li key={i}>{elem.name} {elem.country}</li>})}
+        </ul>
+      </div> */
+
+      <div className="row">
+
+      <div className="col-md-4">
+          Paises
+      </div>
+      <div className="col-md-8">
+          <ul className="list-group">
+              {
+                  this.state.listaPaises.map(pais => <li className="list-group-item list-group-item-action" key={pais._id}>
+                    {pais.name} {pais.country}
+                  </li>)
+              }
+          </ul>
+      </div>
+    
+
+        </div>
+        
+      )
+    }
+};
